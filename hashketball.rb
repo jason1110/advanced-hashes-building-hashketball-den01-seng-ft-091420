a48dd3ab8fb0150aae2523bb823ac89ed1921d85
Jason Easterly <jason.easterly11@gmail.com>
2020-08-28 02:35:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,7 @@ def game_hash(top_hash)
       :blocks => 1,
       :slam_dunks => 1
     },
-    {:player_name => "Reggie Evans",
+     {:player_name => "Reggie Evans",
       :number => "30",
       :shoe => "14",
       :points => 12,
@@ -25,7 +25,7 @@ def game_hash(top_hash)
       :blocks => 12,
       :slam_dunks => 7
     },
-    {:player_name => "Brook Lopez",
+     {:player_name => "Brook Lopez",
       :number => "11",
       :shoe => "17",
       :points => 17,
@@ -35,7 +35,7 @@ def game_hash(top_hash)
       :blocks => 1,
       :slam_dunks => 15
     },
-    {:player_name => "Mason Plumlee",
+     {:player_name => "Mason Plumlee",
       :number => "1",
       :shoe => "19",
       :points => 26,
@@ -45,7 +45,7 @@ def game_hash(top_hash)
       :blocks => 8,
       :slam_dunks => 5
     },
-    {:player_name => "Jason Terry",
+     {:player_name => "Jason Terry",
       :number => "31",
       :shoe => "15",
       :points => 19,
@@ -69,7 +69,7 @@ def game_hash(top_hash)
       :blocks => 0,
       :slam_dunks => 0
     },
-    {:player_name => "",
+     {:player_name => "",
       :number => "",
       :shoe => "",
       :points => 0,
@@ -79,7 +79,7 @@ def game_hash(top_hash)
       :blocks => 0,
       :slam_dunks => 0
     },
-    {:player_name => "",
+     {:player_name => "",
       :number => "",
       :shoe => "",
       :points => 0,
@@ -89,7 +89,7 @@ def game_hash(top_hash)
       :blocks => 0,
       :slam_dunks => 0
     },
-    {:player_name => "",
+     {:player_name => "",
       :number => "",
       :shoe => "",
       :points => 0,
@@ -99,7 +99,7 @@ def game_hash(top_hash)
       :blocks => 0,
       :slam_dunks => 0
     },
-    {:player_name => "",
+     {:player_name => "",
       :number => "",
       :shoe => "",
       :points => 0,
